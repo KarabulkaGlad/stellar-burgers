10441c7baae9762fe7a9c78665896ab133f20abf
Vladislav <vladislavche@bk.ru>
2025-07-29 16:30:54 +0400

    Добавлен auth slice - хранилище для работы с доступом пользователя к ресурсу

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineSlices, configureStore, createReducer } from '@reduxjs/toolkit';
 
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import { authReduser } from './features/auth/auth';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = combineSlices({
+  auth: authReduser,
+});
 
 const store = configureStore({
   reducer: rootReducer,
